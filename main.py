8ece393487038f4912ecc9151cb0753cdbf08380
Martin Li (ml328) <ml328@duke.edu>
2018-02-09 13:54:41 -0500

    fixed files to adhere to pep8

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Assignment 4: TeamOne
 def main():
-    maxMin([2,5,6])
+    maxMin([2, 5, 6])
+
 
 def maxMin(inputList):
     myMin = min(inputList)
     myMax = max(inputList)
-    maxMinTuple = (myMin,myMax)
+    maxMinTuple = (myMin, myMax)
     print(maxMinTuple)
     return maxMinTuple
 
