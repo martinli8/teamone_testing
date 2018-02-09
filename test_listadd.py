188d067105e954c01c395263ac87a59807109b39
David Brenes <db240@duke.edu>
2018-02-09 14:03:06 -0500

    Fix the pep syntax of the test file

--- test_listadd.py
+++ test_listadd.py
@@ -1,9 +1,12 @@
 import pytest
+
+
 def test_listadd():
-	from main import add_list_numbers
-	added_output = add_list_numbers([3,5])
-	added_output_2 = add_list_numbers([-4,10,3])
-	added_output_3 = add_list_numbers([-4, 10.3])
-	assert added_output == 8
-	assert added_output_2 == 9
-	assert added_output_3 == pytest.approx(6.3)
+    from main import add_list_numbers
+    added_output = add_list_numbers([3, 5])
+    added_output_2 = add_list_numbers([-4, 10, 3])
+    added_output_3 = add_list_numbers([-4, 10.3])
+    assert added_output == 8
+    assert added_output_2 == 9
+    assert added_output_3 == pytest.approx(6.3)
+    
