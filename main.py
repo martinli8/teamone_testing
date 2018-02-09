2eba59aa9f20766f905b2b2bf87838c317fcf651
Davidrbr95 <db240@duke.edu>
2018-02-09 14:42:00 -0500

    Fix syntax errors in merging

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ def maxMin(inputList):
     maxMinTuple = (myMin, myMax)
     print(maxMinTuple)
     return maxMinTuple
-     
+    
+    
 def add_list_numbers(list_var):
     final_sum = 0
     for i in list_var:
         final_sum += i
     return final_sum
-  
+    
+    
 if __name__ == "__main__":
     main()
  
