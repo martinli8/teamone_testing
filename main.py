805d2f90a264f6dddd72b6e5f09b2f2cedea9cb6
ethanolnoknow <31057180+ethanolnoknow@users.noreply.github.com>
2018-02-09 14:02:40 -0500

    pep8 standards

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Assignment 4: TeamOne
+def max_diff(my_list):
+    n = 0;
+    for i in range(len(my_list)-1):
+        diff = abs(my_list[i+1] - my_list[i])
+        if diff>n:
+            n = diff
+    return n
