27b4cd06f027a322f97dfb78a42bb4a7cec93b34
Davidrbr95 <db240@duke.edu>
2018-02-09 14:44:27 -0500

    Fix syntax errors martin commit

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ def maxMin(inputList):
     maxMinTuple = (myMin, myMax)
     print(maxMinTuple)
     return maxMinTuple
-    
-    
+
+
 def add_list_numbers(list_var):
     final_sum = 0
     for i in list_var:
         final_sum += i
     return final_sum
-    
-    
+
+
 if __name__ == "__main__":
     main()
- 
