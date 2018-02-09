35601f4a1f79b66c2f64db4a00bd3cf94f6303db
Martin Li (ml328) <ml328@duke.edu>
2018-02-09 13:29:00 -0500

    added unit test and module

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # Assignment 4: TeamOne
+def main():
+    maxMin([2,5,6])
+
+def maxMin(inputList):
+    myMin = min(inputList)
+    myMax = max(inputList)
+    maxMinTuple = (myMin,myMax)
+    print(maxMinTuple)
+    return maxMinTuple
+
+
+if __name__ == "__main__":
+    main()
