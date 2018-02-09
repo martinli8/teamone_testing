101f5455322f76838d3d3798153bdf6a6b8461a6
David Brenes <db240@duke.edu>
2018-02-09 13:30:49 -0500

    Fix syntax error in test_list_addition.py and add variable to function in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Assignment 4: TeamOne
-def add_list_numbers():
+def add_list_numbers(list_var):
 	final_sum = 0
-	for i in add_list_numbers:
+	for i in list_var:
 		final_sum += i
 	return final_sum
+
--- test_list_addition.py
+++
@@ -1,8 +0,0 @@
-def add_list_numbers_test:
-	from main.py import add_list_numbers
-	added_output = add_list_numbers([3,5])
-	added_output_2 = add_list_numbers([-4,10,3])
-	added_output_3 = add_list_numbers([-4, 10.3])
-	assert added_output == 8
-	assert added_output == 9
-	assert added_output_3 = 6.3
