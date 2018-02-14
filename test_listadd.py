6c121a902de19c19d83b9365e27984d4b4a6122b
David Brenes <db240@duke.edu>
2018-02-14 15:13:11 -0500

    Added Xpass and Xfail to both test functions

--- test_listadd.py
+++ test_listadd.py
@@ -1,24 +1,24 @@
 import pytest
 
+@pytest.mark.xfail(reason="xfail if numpy/numbers not installed, else XPASS")
+def test_listadd():
+    """
+    Pytest function for testing the add_list_numbers function in main.py
+    """
+    from main import add_list_numbers
+    added_output = add_list_numbers([3, 5])
+    added_output_2 = add_list_numbers([-4, 10, 3])
+    added_output_3 = add_list_numbers([-4, 10.3])
+    assert added_output == 8
+    assert added_output_2 == 9
+    assert added_output_3 == pytest.approx(6.3)
+    with pytest.raises(TypeError):
+        add_list_numbers([-4, 's'])
+    with pytest.raises(ValueError):
+        add_list_numbers([])
 
-# def test_listadd():
-#     """
-#     Pytest function for testing the add_list_numbers function in main.py
-#     """
-#     from main import add_list_numbers
-#     added_output = add_list_numbers([3, 5])
-#     added_output_2 = add_list_numbers([-4, 10, 3])
-#     added_output_3 = add_list_numbers([-4, 10.3])
-#     assert added_output == 8
-#     assert added_output_2 == 9
-#     assert added_output_3 == pytest.approx(6.3)
-#     with pytest.raises(TypeError):
-#         add_list_numbers([-4, 's'])
-#     with pytest.raises(ValueError):
-#         add_list_numbers([])
 
-
-# @pytest.mark.xfail(reason="Numpy and numbers should be installed")
+@pytest.mark.xfail(reason="XPASS if numpy/numbers not installed, else xfail")
 def test_import_add_list():
     """ Pytests function for testing the add_list_numbers
     for numpy or numbers import error. This test should fail
