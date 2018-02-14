0ff846d362bc7cb899d0a87d89d7c3049ba9e29e
Martin Li (ml328) <ml328@duke.edu>
2018-02-14 14:02:03 -0500

    added docstrings and exceptions

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ def max_diff(my_list):
 
 
 def maxMin(inputList):
+    """
+    Finds the max and min in a list of positive valuesand returns it in a tuple
+
+    :param inputList: Is a list of positive values
+    :returns: Tuple of the max and min values
+    :raises ImportError: If numpy is not installed in the env
+    :raises ValueError: If there are values less than 0
+    :raises TypeError: If the inputList is not an actual list 
+    """
+    try:
+        import numpy
+    except ImportError:
+        raise ImportError('Need to install the Numpy module')
+    for i in inputList:
+        if i < 0:
+            raise ValueError('Negative value detected')
+    if not isinstance(inputList, list):
+        raise TypeError('Input is not a list')
     myMin = min(inputList)
     myMax = max(inputList)
     maxMinTuple = (myMin, myMax)
--- test_maxmin.py
+++ test_maxmin.py
@@ -1,6 +1,18 @@
+import pytest
+
 def test_maxMin():
     from main import maxMin
     testingList = [1, 4, 5, 6, 581]
     test_output_value = (1, 581)
     maxMinOutput = maxMin(testingList)
     assert maxMinOutput == test_output_value
+    with pytest.raises(ValueError):
+        maxMin([1,-4,5])
+    with pytest.raises(TypeError):
+        maxMin((1,4,5))
+
+@pytest.mark.xfail(reason = "Numpy not installed")
+def test_maxMin_import():
+    from main import maxMin
+    with pytest.raises(ImportError):
+        maxMin(1,4,5)
