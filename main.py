f70f2033bcb66083ce540c7ebc27397477ae6427
Martin Li (ml328) <ml328@duke.edu>
2018-02-15 11:19:00 -0500

    pep8 fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Assignment 4: TeamOne
 import logging
 
+
 def max_diff(my_list):
     n = 0
     for i in range(len(my_list)-1):
@@ -21,7 +22,7 @@ def maxMin(inputList):
     :raises TypeError: If the inputList is not an actual list
     """
 
-    logging.basicConfig(filename='log.txt',level=logging.DEBUG)
+    logging.basicConfig(filename='log.txt', level=logging.DEBUG)
 
     try:
         myMin = min(inputList)
@@ -35,9 +36,10 @@ def maxMin(inputList):
         raise ImportError("missing a module!")
     for i in inputList:
         if i < 0:
-            logging.error("negative value detected")
+            logging.warning("Negative value detected")
             raise ValueError('Negative value detected')
     if not isinstance(inputList, list):
+        logging.warning('Input is not a list')
         raise TypeError('Input is not a list')
     logging.info(maxMinTuple)
     return maxMinTuple
--- test_maxmin.py
+++ test_maxmin.py
@@ -7,12 +7,13 @@ def test_maxMin():
     maxMinOutput = maxMin(testingList)
     assert maxMinOutput == test_output_value
     with pytest.raises(ValueError):
-        maxMin([1,-4,5])
+        maxMin([1, -4, 5])
     with pytest.raises(TypeError):
-        maxMin((1,4,5))
+        maxMin((1, 4, 5))
 
-@pytest.mark.xfail(reason = "xfail if Numpy not installed")
+
+@pytest.mark.xfail(reason="xfail if Numpy not installed")
 def test_maxMin_import():
     from main import maxMin
     with pytest.raises(ImportError):
-        maxMin([1,4,5])
+        maxMin([1, 4, 5])
